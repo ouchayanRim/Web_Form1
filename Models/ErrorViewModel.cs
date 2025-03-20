1e208357d822ab332fe68053856ae1c32070e3e5
OuchayanRim <rimouchayan@gmail.com>
2025-03-20 22:04:57 +0000

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Form1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
